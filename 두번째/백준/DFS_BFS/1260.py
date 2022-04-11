c080fc220eb429029a1ae47c37dbb4248f0bd740
yoosumi <>
2022-04-11 23:50:05 +0900

    no message

--- 두번째/백준/DFS_BFS/1260.py
+++ 두번째/백준/DFS_BFS/1260.py
@@ -1,3 +1,41 @@
 import sys
+from collections import deque
 input=sys.stdin.readline
-n,m,v=map
+n,m,v=map(int,input().split())
+graph=[[] for _ in range(n+1)]
+
+for i in range(m):
+    a,b=map(int,input().split())
+    graph[a].append(b)
+for i in graph:
+    i.sort()
+
+flag=[False]*(n+1)
+
+def dfs(node):
+    print(node,end=" ")
+    for i in graph[node]:
+        if flag[i]==False:
+            dfs(i)
+            flag[i]=True
+flag[v]=True
+dfs(v)
+print()
+def bfs(node):
+    while q:
+        now=q.popleft()
+        print(now,end=" ")
+        for i in graph[node]:
+            if flag[i]==False:
+                q.append(i)
+                flag[i]=True
+
+
+flag=[False]*(n+1)
+q=deque()
+q.append(v)
+flag[v]=True
+bfs(v)
+
+
+
